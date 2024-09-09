b3929641edd52baf6ee457c98fe10477c6e4b942
= <=>
2024-09-09 14:14:26 -0300

    Se cambian clases usando bootstrap

--- resources/views/tenant/register.blade.php
+++ resources/views/tenant/register.blade.php
@@ -1,23 +1,27 @@
-<div class="container">
+@extends('layouts.app')
+
+@section('content')
+  <div class="container">
     <form method="POST" action="{{ route('tenant.register') }}">
       @csrf
       <div class="mt-4">
-        <label for="domain" :value="__('Subdomain')" />
-        <div class="flex">
+        <label class="form-label" for="domain" :value="__('Subdomain')" />
+        <div class="d-flex">
           <input
             id="domain"
-            class="mt-1 block w-full"
+            class="form-control"
             type="text"
             name="domain"
             :value="old('domain')"
             required
           />
-          <span class="text-gray-600 ml-2 mt-1">.localhost.com</span>
+          <span class="text-gray-600 ml-2 mt-1">.localhost</span>
         </div>
       </div>
   
       <div class="mt-4 flex items-center justify-end">
-        <button class="ml-4"> {{ __('Register Tenant') }} </button>
+        <button class="ml-4 btn btn-primary"> {{ __('Register Tenant') }} </button>
       </div>
     </form>
   </div>
+@endsection
--- resources/views/tenant/registered.blade.php
+++ resources/views/tenant/registered.blade.php
@@ -1,17 +1,21 @@
-<div>
-    <div class="text-gray-600 mb-4 text-sm">
+@extends('layouts.app')
+
+@section('content')
+  <div class="container">
+    <div class="mb-4 text-sm">
       {{ __('Your tenant has been registered successfully!') }}
     </div>
   
-    <div class="mt-4 flex items-center justify-between">
+    <div class="mt-4 d-flex items-center justify-between">
       <div>
         Your tenant URL:
         <a
           href="http://{{ $domain }}.localhost.com"
-          class="text-gray-600 hover:text-gray-900 text-sm underline"
+          class="text-sm underline"
           target="_blank"
           >http://{{ $domain }}.localhost.com</a
         >
       </div>
     </div>
   </div>
+@endsection
